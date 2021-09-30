78f0d8b4893afe20e61f6842a8989ee9af27796b
GiuseppeRaso <54646766+GiuseppeRaso@users.noreply.github.com>
2021-09-30 17:01:51 +0200

    Update db-container-backup.sh
    - all databases
    - change folder and days
    - remove bitwarden

--- db-container-backup/db-container-backup.sh
+++ db-container-backup/db-container-backup.sh
@@ -6,8 +6,8 @@
 # It currently supports mariadb, mysql and bitwardenrs containers.
 # 
 
-DAYS=2
-BACKUPDIR=/home/xcad/backup
+DAYS=7
+BACKUPDIR=/root/db_backups
 
 
 # backup all mysql/mariadb containers
@@ -21,30 +21,14 @@ if [ ! -d $BACKUPDIR ]; then
 fi
 
 for i in $CONTAINER; do
-    MYSQL_DATABASE=$(docker exec $i env | grep MYSQL_DATABASE |cut -d"=" -f2)
     MYSQL_PWD=$(docker exec $i env | grep MYSQL_ROOT_PASSWORD |cut -d"=" -f2)
 
-    docker exec -e MYSQL_DATABASE=$MYSQL_DATABASE -e MYSQL_PWD=$MYSQL_PWD \
-        $i /usr/bin/mysqldump -u root $MYSQL_DATABASE \
-        | gzip > $BACKUPDIR/$i-$MYSQL_DATABASE-$(date +"%Y%m%d%H%M").sql.gz
+    docker exec \
+        $i /usr/bin/mysqldump -u root -p $MYSQL_PWD --all-databases \
+        | gzip > $BACKUPDIR/$i-DB-$(date +"%Y%m%d%H%M").sql.gz
 
     OLD_BACKUPS=$(ls -1 $BACKUPDIR/$i*.gz |wc -l)
     if [ $OLD_BACKUPS -gt $DAYS ]; then
-        find $BACKUPDIR -name "$i*.gz" -daystart -mtime +$DAYS -delete
-    fi
-done
-
-
-# bitwarden backup
-
-BITWARDEN_CONTAINERS=$(docker ps --format '{{.Names}}:{{.Image}}' | grep 'bitwardenrs' | cut -d":" -f1)
-
-for i in $BITWARDEN_CONTAINERS; do
-    docker exec  $i /usr/bin/sqlite3 data/db.sqlite3 .dump \
-        | gzip > $BACKUPDIR/$i-$(date +"%Y%m%d%H%M").sql.gz
-
-    OLD_BITWARDEN_BACKUPS=$(ls -1 $BACKUPDIR/$i*.gz |wc -l)
-    if [ $OLD_BITWARDEN_BACKUPS -gt $DAYS ]; then
         find $BACKUPDIR -name "$i*.gz" -daystart -mtime +$DAYS -delete
     fi
 done
